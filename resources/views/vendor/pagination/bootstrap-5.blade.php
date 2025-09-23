bada41badcca8055c858d133f42fbc4eddec3338
Vova Krupenkin <vov.krupenkin@gmail.com>
2025-09-23 23:34:45 +0300

    fix(pagination): исправлена пагинация на русском языке - создан кастомный шаблон bootstrap-5.blade.php - переведены кнопки 'редыдущая'/'Следующая' - добавлена информация о количестве результатов - стилизована пагинация в дизайне арелии

---
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -0,0 +1,57 @@
+@if ($paginator->hasPages())
+    <nav>
+        <ul class="pagination justify-content-center">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">&lsaquo; Предыдущая</span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo; Предыдущая</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        @else
+                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">Следующая &rsaquo;</a>
+                </li>
+            @else
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link" aria-hidden="true">Следующая &rsaquo;</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+
+    {{-- Pagination Info --}}
+    <div class="d-flex justify-content-center mt-3">
+        <p class="text-muted small mb-0">
+            @if($paginator->total() > 0)
+                Показано {{ $paginator->firstItem() }} - {{ $paginator->lastItem() }} из {{ $paginator->total() }} результатов
+            @else
+                Результаты не найдены
+            @endif
+        </p>
+    </div>
+@endif
